6a8848b3dba758384fa69b6780903b5349daaeb8
junyoung1 <69309482+junyoung1@users.noreply.github.com>
2021-11-30 10:33:38 +0900

    적외선 적용
    초음파센서와 적외선센서를 이용한 모터구동

--- LineTracer/LineTracer.ino
+++ LineTracer/LineTracer.ino
@@ -4,7 +4,9 @@
 
 Servo servoLeft;                             // Declare left and right servos
 Servo servoRight;
- 
+int senceRpin = 7;  // This is our input pin
+int senceR = HIGH;  // HIGH MEANS NO OBSTACLE
+
 void setup()                                 // Built-in initialization block
 {
   Serial.begin (9600);              // 시리얼 모니터를 사용하기 위해 보드레이트를 9600으로 설정합니다.
@@ -12,7 +14,7 @@ void setup()                                 // Built-in initialization block
   pinMode(echoPin, INPUT);    // echoPin 핀을 입력핀으로 설정합니다.
   tone(4, 3000, 1000);                       // Play tone for 1 second
   delay(1000);                               // Delay to finish tone
-
+  pinMode(senceRpin, INPUT);           //적외선 센서
   servoLeft.attach(13);                      // Attach left signal to pin 13
   servoRight.attach(12);                     // Attach right signal to pin 12
 
@@ -45,19 +47,27 @@ void loop()
   delayMicroseconds(10);                  // trigPin을 10마이크로초 동안 기다렸다가
   digitalWrite(trigPin, LOW);                // trigPin에 LOW를 출력합니다.
   duration = pulseIn(echoPin, HIGH);   // echoPin핀에서 펄스값을 받아옵니다.
-distance = duration * 17 / 1000;          //  duration을 연산하여 센싱한 거리값을 distance에 저장합니다.
+  distance = duration * 17 / 1000;          //  duration을 연산하여 센싱한 거리값을 distance에 저장합니다.
 
   if (distance >= 15)       // 거리가 15cm가 넘으면
   {
-       servoLeft.writeMicroseconds(1700);         // 전진
-      servoRight.writeMicroseconds(1300);  
+    servoLeft.writeMicroseconds(1700);         // 전진
+    servoRight.writeMicroseconds(1300);
+    senceR = digitalRead(senceRpin);
+    if (senceR == LOW) { 
+      servoLeft.writeMicroseconds(1700);         // 우회전
+      servoRight.writeMicroseconds(1530);   
+    }
+    else {
+      servoLeft.writeMicroseconds(1700);         // 전진
+      servoRight.writeMicroseconds(1300);
+    } 
   }
   else                                                    // 거리가 200cm가 넘지 않거나 0보다 작지 않으면
   {
-    Serial.print(distance);                         // distance를 시리얼 모니터에 출력합니다.
-    Serial.println(" cm");                           // cm를 출력하고 줄을 넘깁니다.
- servoLeft.writeMicroseconds(1525);         // 멈춤
-  servoRight.writeMicroseconds(1530);
+    servoLeft.writeMicroseconds(1525);         // 멈춤
+    servoRight.writeMicroseconds(1530);
   }
-  delay(500);                                         // 0.5초동안 기다립니다.
-}
+    delay(500);
+  
+  }
