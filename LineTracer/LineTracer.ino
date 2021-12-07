23da42184ad351b3c9f4669414c0ac02b739fed0
junyoung1 <69309482+junyoung1@users.noreply.github.com>
2021-12-07 10:19:16 +0900

    적외선 왼쪽 추가

--- LineTracer/LineTracer.ino
+++ LineTracer/LineTracer.ino
@@ -6,6 +6,8 @@ Servo servoLeft;                             // Declare left and right servos
 Servo servoRight;
 int senceRpin = 7;  // This is our input pin
 int senceR = HIGH;  // HIGH는 장애물이 없음을 뜻함
+int senceLpin = 6;  // This is our input pin
+int senceL = HIGH;  // HIGH는 장애물이 없음을 뜻함
 
 void setup() {
   Serial.begin (9600);              // 시리얼 모니터를 사용하기 위해 보드레이트를 9600으로 설정합니다.
@@ -14,6 +16,7 @@ void setup() {
   tone(4, 3000, 1000);                       // Play tone for 1 second
   delay(1000);                               // Delay to finish tone
   pinMode(senceRpin, INPUT);           //적외선 센서
+  pinMode(senceLpin, INPUT);  
   servoLeft.attach(13);                      // Attach left signal to pin 13
   servoRight.attach(12);                     // Attach right signal to pin 12
 
@@ -45,7 +48,15 @@ void loop() {
     else {
       servoLeft.writeMicroseconds(1700);         // 전진
       servoRight.writeMicroseconds(1300);
-    } 
+    }
+    if (senceL == LOW){
+    servoLeft.writeMicroseconds(1525);         // 우회전
+      servoRight.writeMicroseconds(1300);   
+    }
+    else {
+      servoLeft.writeMicroseconds(1700);         // 전진
+      servoRight.writeMicroseconds(1300);
+    }
   }
   else { // 거리가 15cm가 넘지 않거나 0보다 작지 않으면
     servoLeft.writeMicroseconds(1525);         // 멈춤
